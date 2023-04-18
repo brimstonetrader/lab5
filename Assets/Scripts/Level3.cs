43247a6ea239a83d9a1ad6c621102553eb2cd3d3
Tucker Brock <brocktk@hendrix.edu>
2023-04-17 20:37:42 -0500

    Set up Levels 2 and 3 scripts

---
+++ Assets/Scripts/Level3.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Level3 : MonoBehaviour {
+
+	public GameObject target;
+	public float speed;
+	public float rotationSpeed;
+
+	private Rigidbody body;
+
+    void Awake()
+    {
+        StartCoroutine(SwanSpeed());
+    }
+
+    void Start () {
+		body = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 desired = (target.transform.position - transform.position).normalized;
+		body.AddForce(desired * speed - body.velocity);
+
+		float angle = (Mathf.Atan2(desired.y, desired.x) * Mathf.Rad2Deg) - 90;
+		Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+		transform.rotation = Quaternion.Slerp(transform.rotation,
+			q, Time.deltaTime * rotationSpeed);
+
+	}
+
+	public void IncreaseSpeed() {
+		speed *= 1.2f;
+	}
+
+	void OnCollisionEnter(Collision coll) {
+        if (coll.gameObject == target)
+        {
+            GetComponent<AudioSource>().Play();
+            SceneManager.LoadScene("Death_scene");
+        }
+    }
+
+
+    IEnumerator SwanSpeed()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(3);
+            IncreaseSpeed();
+			// After 60 seconds, the speed will be 10+.
+			// This basically makes the game unplayable. So the player has one minute to make it to the cd
+        }
+    }
+
+}
