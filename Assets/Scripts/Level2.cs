6a709a55548a336c9a9eb3720a894882590b0980
Tucker Brock <brocktk@hendrix.edu>
2023-04-19 12:41:11 -0500

    Level 2 script almost working

--- Assets/Scripts/Level2.cs
+++ Assets/Scripts/Level2.cs
@@ -1,59 +1,104 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine.SceneManagement;
+// using UnityEngine;
+
+// public class Level2 : MonoBehaviour {
+
+// 	public GameObject target;
+// 	public float speed;
+// 	public float rotationSpeed;
+
+// 	private Rigidbody body;
+
+//     void Awake()
+//     {
+//         StartCoroutine(VirusSpeed());
+//     }
+
+//     void Start () {
+// 		body = GetComponent<Rigidbody>();
+// 	}
+	
+// 	// Update is called once per frame
+// 	void FixedUpdate () {
+// 		Vector3 desired = (target.transform.position - transform.position).normalized;
+// 		body.AddForce(desired * speed - body.velocity);
+
+// 		float angle = (Mathf.Atan2(desired.y, desired.x) * Mathf.Rad2Deg) - 90;
+// 		Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+// 		transform.rotation = Quaternion.Slerp(transform.rotation,
+// 			q, Time.deltaTime * rotationSpeed);
+
+// 	}
+
+// 	public void IncreaseSpeed() {
+// 		speed *= 1.2f;
+// 	}
+
+// 	void OnCollisionEnter(Collision coll) {
+//         if (coll.gameObject == target)
+//         {
+//             GetComponent<AudioSource>().Play();
+//             SceneManager.LoadScene("Death_scene");
+//         }
+//     }
+
+
+//     IEnumerator VirusSpeed()
+//     {
+//         while (true)
+//         {
+//             yield return new WaitForSeconds(3);
+//             IncreaseSpeed();
+// 			// After 60 seconds, the swan speed will be 10+.
+// 			// This basically makes the game unplayable. So the player has one minute to make it to the cave
+//         }
+//     }
+
+// }
+
+
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class Level2 : MonoBehaviour {
+public class Level2 : MonoBehaviour{
+    public float speed;
+    public float distance;
 
-	public GameObject target;
-	public float speed;
-	public float rotationSpeed;
+    private Vector3 startPosition;
+    private Vector3 endPosition;
+    private Vector3 direction;
+    private float travelTime;
 
-	private Rigidbody body;
-
-    void Awake()
-    {
-        StartCoroutine(VirusSpeed());
+    void Start(){
+        startPosition = transform.position;
+        endPosition = startPosition + new Vector3(distance, 0, 0);
+        direction = (endPosition - startPosition).normalized;
+        travelTime = distance / speed;
+        StartCoroutine(Move());
     }
 
-    void Start () {
-		body = GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		Vector3 desired = (target.transform.position - transform.position).normalized;
-		body.AddForce(desired * speed - body.velocity);
-
-		float angle = (Mathf.Atan2(desired.y, desired.x) * Mathf.Rad2Deg) - 90;
-		Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
-		transform.rotation = Quaternion.Slerp(transform.rotation,
-			q, Time.deltaTime * rotationSpeed);
-
-	}
-
-	public void IncreaseSpeed() {
-		speed *= 1.2f;
-	}
-
-	void OnCollisionEnter(Collision coll) {
-        if (coll.gameObject == target)
-        {
-            GetComponent<AudioSource>().Play();
-            SceneManager.LoadScene("Death_scene");
+    IEnumerator Move(){
+        while (true){
+            yield return StartCoroutine(MoveToPosition(endPosition, travelTime));
+            yield return StartCoroutine(MoveToPosition(startPosition, travelTime));
         }
     }
 
+    IEnumerator MoveToPosition(Vector3 targetPosition, float time){
+        float elapsedTime = 0;
 
-    IEnumerator VirusSpeed()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(3);
-            IncreaseSpeed();
-			// After 60 seconds, the swan speed will be 10+.
-			// This basically makes the game unplayable. So the player has one minute to make it to the cave
+        Vector3 startingPosition = transform.position;
+
+        while (elapsedTime < time){
+            transform.position = Vector3.Lerp(startingPosition, targetPosition, (elapsedTime / time));
+            elapsedTime += Time.deltaTime;
+            yield return null;
         }
-    }
 
+        transform.position = targetPosition;
+    }
 }
+
