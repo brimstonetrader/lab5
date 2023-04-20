6efcbd1a7082befa2d9d8f7b39bd519f11a6ebc9
brimstonetrader <106700187+brimstonetrader@users.noreply.github.com>
2023-04-20 17:13:48 -0500

    sound;animation

--- Assets/Scripts/FPSMovement.cs
+++ Assets/Scripts/FPSMovement.cs
@@ -16,9 +16,6 @@ public class FPSMovement : MonoBehaviour
     private float horizontal;
     private float vertical;
     
-    private float moveLimiter = 0.7f;
-
-    public float runSpeed = 5f;
 
     void Start () {
         animator = body.GetComponent<Animator>();
@@ -27,48 +24,59 @@ public class FPSMovement : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {           
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
-
-        // when you want to jump
-        if (Input.GetKeyDown(KeyCode.Space) && grounded)
-        {
-            grounded = false;
-            rigidbody.AddRelativeForce(new Vector3(0, 5f, 0), ForceMode.Impulse);
-            animator.SetTrigger("jump");
+        if ((horizontal != 0f) | (vertical != 0f)) {
+            animator.SetFloat("speed", 1);
         }
+
+
+        if ((horizontal == 0) & (vertical == 0)) {
+            animator.SetFloat("speed", 0);
+        }
+
+        // when you want to dance
+        if (Input.GetKeyDown(KeyCode.N) && grounded)
+        {
+            animator.SetTrigger("breakdance");
+        }
+
+        if (Input.GetKeyDown(KeyCode.M) && grounded)
+        {
+            animator.SetTrigger("cancan");
+        }
+
+        if (Input.GetKeyDown(KeyCode.J) && grounded)
+        {
+            animator.SetTrigger("standard");
+        }
+
+        if (Input.GetKeyDown(KeyCode.K) && grounded)
+        {
+            animator.SetTrigger("hiphop");
+        }
+
+        if (Input.GetKeyDown(KeyCode.L) && grounded)
+        {
+            animator.SetTrigger("twerk");
+        }
+
+        if (Input.GetKeyDown(KeyCode.U) && grounded)
+        {
+            animator.SetTrigger("gangnam");
+        }
+
+        if (Input.GetKeyDown(KeyCode.I) && grounded)
+        {
+            animator.SetTrigger("flair");
+        }
+
+        if (Input.GetKeyDown(KeyCode.O) && grounded)
+        {
+            animator.SetTrigger("flair");
+        }
+
     }
 
-    void FixedUpdate() {
-        if (horizontal != 0 && vertical != 0) {
-            horizontal *= moveLimiter;
-            vertical *= moveLimiter;
-        }
-        Vector3 v = new Vector3(horizontal * runSpeed, 0, vertical * runSpeed);
-        animator.SetFloat("speed", v.sqrMagnitude);
-        print(v);
-        if (v != Vector3.zero)
-        {
-            gameObject.transform.forward = v;
-        }
-        rigidbody.velocity = v + new Vector3(0, rigidbody.velocity.y, 0);
-
-    }
-
-    /// <summary>
-    /// Check for collision back to the ground, and re-enable the NavMeshAgent
-    /// </summary>
-    private void OnCollisionEnter(Collision collision)
-    {
-        print("Hit Something!");
-        if (collision.collider != null && collision.collider.tag == "Ground")
-        {
-            print("It is the ground!");
-            if (!grounded)
-            {
-                grounded = true;
-            }
-        }
-    }
 }
